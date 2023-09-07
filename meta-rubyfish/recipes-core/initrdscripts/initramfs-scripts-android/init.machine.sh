f774a524dc0c2e5169b31716d3ebe8cc7f10355e
Philip Russell <argosphil@murena.io>
2023-09-07 21:45:45 +0000

    mount the "system" partition at /androidroot since /system is /androidroot/system

--- meta-rubyfish/recipes-core/initrdscripts/initramfs-scripts-android/init.machine.sh
+++ meta-rubyfish/recipes-core/initrdscripts/initramfs-scripts-android/init.machine.sh
@@ -2,6 +2,13 @@
 
 BOOT_DIR=$1
 
+# Manually mount the system partition as it contains the boot image (ramdisk) as well as the system data (under /system/system).
+mkdir -m 0777 $BOOT_DIR/androidroot
+mount -t auto -o ro /dev/mmcblk0p29 $BOOT_DIR/androidroot
+
+# Make the 'system' folder available as the system partition to the rootfs.
+ln -s /androidroot/system/ $BOOT_DIR/system
+
 # Check if the AsteroidOS specific machine configuration file exists.
 # If it doesn't then we know that the userdata partition is mounted but doesn't contain a valid AsteroidOS root.
 if [ ! -e $BOOT_DIR/etc/asteroid/machine.conf ] ; then
